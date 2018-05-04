1269039ee7ea3bac83759d4ef06f81445a4ba539
Jacky Masset <halloakinator@gmail.com>
2018-05-04 21:46:01 +0200

    added js branch

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python BotA.py
+web: node index.js
--- const.py
+++
@@ -1,9 +0,0 @@
-import os
-from boto.s3.connection import S3Connection
-BOT_PREFIX = "bota "
-
-def index():
-    TOKEN = str(os.environ.get('TOKEN'))
-    return TOKEN
-
-TOKEN = index()
